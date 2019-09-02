c44ad32863a5b1f135e08d4935c86463d8a858bb
Austin Asbury <acasbury25@aol.com>
2019-09-02 02:58:35 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,9 +1,17 @@
 module Memorable
-  def count
-    all.count
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
   end
 
-  def reset_all
-    all.clear
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
   end
 end
